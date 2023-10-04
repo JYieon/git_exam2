4e9b7392b7cff521ec3ccb8e63d0fcbd95b3cf7a
Choi Jiyeon <dkcl.8642@gmail.com>
2023-10-04 14:49:57 +0900

    202114069-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,6 +3,7 @@ package git_exam2;
 public class Hello {
 	
 	public static void main(String[] args) {
+		System.out.println("IT웅합자율학부");
 		System.out.println("202114069");
 		System.out.println("홍길동");
 	}
